eebd11c6d5195604baf4960f8331dd02fe9169ba
rian <19516527+360ied@users.noreply.github.com>
2021-02-11 21:31:42 -0500

    add more operators

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,31 @@
 module Main where
 
+import Debug.Trace
 import System.Environment (getArgs)
 import Text.Read (readMaybe)
 
 handleOperator :: Floating a => [Char] -> [a] -> [a]
+handleOperator op [] =
+  case op of
+    "pi" -> [pi]
+handleOperator op [a] =
+  case op of
+    "exp" -> [exp a]
+    "log" -> [log a]
+    "sqrt" -> [sqrt a]
+    "sin" -> [sin a]
+    "cos" -> [cos a]
+    "tan" -> [tan a]
+    "asin" -> [asin a]
+    "acos" -> [acos a]
+    "atan" -> [atan a]
+    "sinh" -> [sinh a]
+    "cosh" -> [cosh a]
+    "tanh" -> [tanh a]
+    "asinh" -> [asinh a]
+    "acosh" -> [acosh a]
+    "atanh" -> [atanh a]
+    _ -> handleOperator op [] ++ [a]
 handleOperator op (a : b : s) =
   case op of
     "+" -> b + a : s
@@ -11,6 +33,7 @@ handleOperator op (a : b : s) =
     "*" -> b * a : s
     "/" -> b / a : s
     "^" -> b ** a : s
+    _ -> handleOperator op [a] ++ b : s
 
 exec :: [String] -> [Double] -> [Double]
 exec [] stack = stack
@@ -22,6 +45,7 @@ exec (token : tokens) stack =
 main :: IO ()
 main = do
   args <- getArgs
+  print $ "args: " ++ foldl1 (\a b -> a ++ " " ++ b) args
   let stack = exec args []
   if length stack == 1
     then print $ head stack
